4ecbc4c720cf1cf382d5e3546c484569b0f15073
IsmaCortGtz <ismacortgtz@gmail.com>
2021-12-01 16:02:49 -0600

    PWA Fixed, or i think so

--- sw.js
+++ sw.js
@@ -1,16 +1,68 @@
-self.addEventListener("install", function(event){
-	event.waitUntil(
-	 	caches.open("sw-cache").then(function(cache){;
-			return cache.add("index.html", "scripts/events.js", "scripts/index.js", "scripts/modules.js", "styles/content-counter.css", "content-list.css", "styles/global.css", "styles/header.css", "styles/options.css", "styles/responsive.css");
-		});
-	); 
-});
+var URLS = [
+	"/",
+	"/PomodoroTechnique/",
+	"/PomodoroTechnique/index.html",
+	"/PomodoroTechnique/scripts/events.js",
+	"/PomodoroTechnique/scripts/index.js",
+	"/PomodoroTechnique/scripts/modules.js",
+	"/PomodoroTechnique/styles/content-counter.css",
+	"/PomodoroTechnique/styles/content-list.css",
+	"/PomodoroTechnique/styles/global.css",
+	"/PomodoroTechnique/styles/header.css",
+	"/PomodoroTechnique/styles/options.css",
+	"/PomodoroTechnique/styles/responsive.css"
+];
+var APP_PREFIX = 'PomodoroTechnique_'     // Identifier for this app (this needs to be consistent across every cache update)
+var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
+var CACHE_NAME = APP_PREFIX + VERSION
 
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
 
-self.addEventListener("fetch", function(event){
-	event.respondWith(
-		caches.match(event.request).then(function(response){
-			return response || fetch(event.request);
-		}); 
-	);
-});
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
+    })
+  )
+})
+
+// Cache resources
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(URLS)
+    })
+  )
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create white list
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      // add current cache name to white list
+      cacheWhitelist.push(CACHE_NAME)
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] )
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
