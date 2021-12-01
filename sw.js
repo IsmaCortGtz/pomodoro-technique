568280b262d70f8a72fe8e1136f2f3da8ca16175
IsmaCortGtz <ismacortgtz@gmail.com>
2021-12-01 16:52:46 -0600

    PWA Fixed, test number 2

--- sw.js
+++ sw.js
@@ -1,19 +1,5 @@
+
 /* var filesToCache = [
-	"/",
-	"/PomodoroTechnique/",
-	"/PomodoroTechnique/index.html",
-	"/PomodoroTechnique/scripts/events.js",
-	"/PomodoroTechnique/scripts/index.js",
-	"/PomodoroTechnique/scripts/modules.js",
-	"/PomodoroTechnique/styles/content-counter.css",
-	"/PomodoroTechnique/styles/content-list.css",
-	"/PomodoroTechnique/styles/global.css",
-	"/PomodoroTechnique/styles/header.css",
-	"/PomodoroTechnique/styles/options.css",
-	"/PomodoroTechnique/styles/responsive.css"
-]; */
-var cacheName = 'PomodoroTechnique_cache';
-var filesToCache = [
 	"./",
 	"./index.html",
 	"./scripts/events.js",
@@ -25,23 +11,61 @@ var filesToCache = [
 	"./styles/header.css",
 	"./styles/options.css",
 	"./styles/responsive.css"
+]; */
+
+let cacheName = "PomodoroTechnique_cache";// 👈 any unique name
+let filesToCache = [
+  "/PomodoroTechnique/", // 👈 your repository name , both slash are important
+  "sw.html",
+	"scripts/events.js",
+	"scripts/index.js",
+	"scripts/modules.js",
+	"styles/content-counter.css",
+	"styles/content-list.css",
+	"styles/global.css",
+	"styles/header.css",
+	"styles/options.css",
+	"styles/responsive.css",
+  "manifest.json",
+  "sources/icon.png"
+  // add your assets here 
 ];
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-  self.skipWaiting();
+self.addEventListener("install", function (event) {
+  event.waitUntil(caches.open(cacheName).then((cache) => {
+    console.log('installed successfully')
+    return cache.addAll(filesToCache);
+  }));
 });
 
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+self.addEventListener('fetch', function (event) {
+
+  if (event.request.url.includes('clean-cache')) {
+    caches.delete(cacheName);
+    console.log('Cache cleared')
+  }
+
+  event.respondWith(caches.match(event.request).then(function (response) {
+    if (response) {
+      console.log('served form cache')
+    } else {
+      console.log('Not serving from cache ', event.request.url)
+    }
+    return response || fetch(event.request);
+  })
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function (key) {
+        if (key !== cacheName) {
+          console.log('service worker: Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
     })
   );
+  return self.clients.claim();
 });
