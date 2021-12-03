11df44b34841acdb99c35a73250109f6e327b5d7
IsmaCortGtz <ismacortgtz@gmail.com>
2021-12-03 14:49:19 -0600

    Icon changed

--- sw.js
+++ sw.js
@@ -1,40 +1,40 @@
 var URLS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './sw.js',
-  './scripts/events.js',
-  './scripts/index.js',
-  './scripts/modules.js',
-  './styles/content-counter.css',
-  './styles/content-list.css',
-  './styles/global.css',
-  './styles/header.css',
-  './styles/options.css',
-  './styles/responsive.css',
-  './sources/icon.png',
-  './sources/alarm.svg',
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js",
+  "./scripts/events.js",
+  "./scripts/index.js",
+  "./scripts/modules.js",
+  "./styles/content-counter.css",
+  "./styles/content-list.css",
+  "./styles/global.css",
+  "./styles/header.css",
+  "./styles/options.css",
+  "./styles/responsive.css",
+  "./sources/icon.png",
+  "./sources/alarm.svg",
   "./sources/default-alarm.mp3",
-  './sources/exit.svg',
-  './sources/pause.svg',
-  './sources/play.svg',
-  './sources/settings.svg'
+  "./sources/exit.svg",
+  "./sources/pause.svg",
+  "./sources/play.svg",
+  "./sources/settings.svg"
 ];
 
-var APP_PREFIX = 'PomodoroTechnique_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_04'              // Version of the off-line cache (change this value everytime you want to update cache)
+var APP_PREFIX = "PomodoroTechnique_"     // Identifier for this app (this needs to be consistent across every cache update)
+var VERSION = "version_05"              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 
 // Respond with cached resources
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
+self.addEventListener("fetch", function (e) {
+  console.log("fetch request : " + e.request.url)
   e.respondWith(
     caches.match(e.request).then(function (request) {
       if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
+        console.log("responding with cache : " + e.request.url)
         return request
       } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
+        console.log("file is not cached, fetching : " + e.request.url)
         return fetch(e.request)
       }
 
@@ -45,17 +45,17 @@ self.addEventListener('fetch', function (e) {
 })
 
 // Cache resources
-self.addEventListener('install', function (e) {
+self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
+      console.log("installing cache : " + CACHE_NAME)
       return cache.addAll(URLS)
     })
   )
 })
 
 // Delete outdated caches
-self.addEventListener('activate', function (e) {
+self.addEventListener("activate", function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
       // `keyList` contains all cache names under your username.github.io
@@ -68,7 +68,7 @@ self.addEventListener('activate', function (e) {
 
       return Promise.all(keyList.map(function (key, i) {
         if (cacheWhitelist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i] )
+          console.log("deleting cache : " + keyList[i] )
           return caches.delete(keyList[i])
         }
       }))
