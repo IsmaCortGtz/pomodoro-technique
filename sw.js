d82ee16394ca1587556a5883991a4ebbed56a40e
IsmaCortGtz <ismacortgtz@gmail.com>
2021-12-03 14:42:24 -0600

    Icon changed

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@ var URLS = [
   './styles/responsive.css',
   './sources/icon.png',
   './sources/alarm.svg',
-  './sources/default-alarm.mp3',
+  "./sources/default-alarm.mp3",
   './sources/exit.svg',
   './sources/pause.svg',
   './sources/play.svg',
@@ -22,7 +22,7 @@ var URLS = [
 ];
 
 var APP_PREFIX = 'PomodoroTechnique_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_03'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_04'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 
 // Respond with cached resources
